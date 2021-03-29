10cfa86b262b7eca6ea8facd742a4e8d4a8ceda9
Matthew Evers <mevers@pivotint.com>
2021-03-29 14:26:56 -0500

    feat: move downloaded files to gallery

--- flutter_vlc_player/example/lib/main.dart
+++ flutter_vlc_player/example/lib/main.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 import 'multiple_tab.dart';
 import 'single_tab.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:gallery_saver/gallery_saver.dart';
 import 'dart:io';
 import 'dart:developer' as developer;
 void tt() async {
   WidgetsFlutterBinding.ensureInitialized();
   final Directory directory = await getApplicationSupportDirectory();
+  List<File> files = [];
   await for (var entity in directory.list(recursive: true, followLinks: false)) 
   {
-    print(entity.path);
+    if (entity.path.endsWith('.mp4')) {
+      var success = await GallerySaver.saveVideo(entity.path);
+      if (success && entity is File) {
+        files.add(entity);
+      }
+    }
+  }
+
+  for (var file in files) {
+    await file.delete();
   }
 }
 
